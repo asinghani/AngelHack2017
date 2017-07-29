887a9f09d08fe2e8b5271b5fa6b3f652ae64cbb1
asinghani <asinghani@noreply.users.github.com>
2017-07-29 11:07:38 -0700

    commit 11:07 AM

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var userMarker;
 var map;
 
+var zoomed = false;
+
+var programmaticMove = false;
+
 $(document).ready(() => {
     $("#map").height(window.innerHeight);
 
     map = L.map("map", {
         zoomControl: false
     }).setView([37, -122], 5);
+
+    createLocateButton();
 
     map.addControl(L.control.zoom({position: "bottomright"}));
 
@@ -25,6 +31,21 @@ $(document).ready(() => {
 
         navigator.geolocation.watchPosition(showLocation, console.error, options);
     }
+
+    //L.control.watermark({position: "bottomleft"}).addTo(map);
+
+
+    map.on("zoomstart", () => {
+        if(!programmaticMove) {
+            zoomed = false;
+        }
+    });
+
+    map.on("dragstart", () => {
+        if(!programmaticMove) {
+            zoomed = false;
+        }
+    });
 });
 
 function showLocation(location) {
@@ -32,10 +53,48 @@ function showLocation(location) {
     let coords = [location.coords.latitude, location.coords.longitude];
     if(!userMarker) {
         userMarker = L.marker(coords).addTo(map);
+        programmaticMove = true;
         map.setZoom(17);
         map.panTo(coords);
+        programmaticMove = false;
+        zoomed = true;
     } else {
         userMarker.setLatLng(coords);
-        map.panTo(coords);
+        if(zoomed) {
+            programmaticMove = true;
+            map.panTo(coords);
+            programmaticMove = false;
+        }
     }
 }
+
+
+function createLocateButton() {
+    L.Control.Locate = L.Control.extend({
+        onAdd: function(map) {
+            var img = L.DomUtil.create('img');
+
+            img.src = '../../docs/images/logo.png';
+            img.style.width = '200px';
+
+            return img;
+        }
+    });
+
+    L.control.locate = (opts) => new L.Control.Locate(opts);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
