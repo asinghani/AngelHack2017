de4eda955ba12f42ee8fda776440ea4d7ef02864
asinghani <asinghani@noreply.users.github.com>
2017-07-30 10:51:19 -0700

    changes

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 var proxy = require("redbird")({ssl: {port: 443}});
 
-require("redbird").register("find-safe.space", "http://localhost:3000", {
+redbird.register("find-safe.space", "http://localhost:3000", {
     ssl: {
         letsencrypt: {
             email: "anish.singhani@gmail.com",
@@ -10,7 +10,7 @@ require("redbird").register("find-safe.space", "http://localhost:3000", {
     }
 });
 
-require("redbird").register("www.find-safe.space", "http://localhost:3000", {
+redbird.register("www.find-safe.space", "http://localhost:3000", {
     ssl: {
         letsencrypt: {
             email: "anish.singhani@gmail.com",
@@ -20,7 +20,7 @@ require("redbird").register("www.find-safe.space", "http://localhost:3000", {
     }
 });
 
-require("redbird").register("gotosafe.space", "http://localhost:3000", {
+redbird.register("gotosafe.space", "http://localhost:3000", {
     ssl: {
         letsencrypt: {
             email: "anish.singhani@gmail.com",
@@ -30,7 +30,7 @@ require("redbird").register("gotosafe.space", "http://localhost:3000", {
     }
 });
 
-require("redbird").register("www.gotosafe.space", "http://localhost:3000", {
+redbird.register("www.gotosafe.space", "http://localhost:3000", {
     ssl: {
         letsencrypt: {
             email: "anish.singhani@gmail.com",
