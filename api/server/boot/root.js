0c63b06b4b86ff7e22dbd6618c28bee682672bf3
asinghani <asinghani@noreply.users.github.com>
2017-07-30 10:20:42 -0700

    Fixes to the homepage

--- api/server/boot/root.js
+++ api/server/boot/root.js
@@ -3,6 +3,6 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res) => res.redirect("/ui/"));
   server.use(router);
 };
