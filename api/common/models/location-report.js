4880cbad105c8fba4342feef5cab85c8e6feb325
asinghani <asinghani@noreply.users.github.com>
2017-07-30 13:26:55 -0700

    Deploying

--- api/common/models/location-report.js
+++ api/common/models/location-report.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Locationreport) {
 
+  Locationreport.validatesInclusionOf('generalSafety', {
+    in: [1,2,3,4,5,6,7,8,9,10]
+  });
+
+
+  Locationreport.validatesInclusionOf('cleanliness', {
+    in: [1,2,3,4,5,6,7,8,9,10]
+  });
+
+  Locationreport.validatesLengthOf('comment', {min: 3});
+
+  Locationreport.validatesLengthOf('locationId', {min: 3});
 };
--- api/common/models/report.js
+++ api/common/models/report.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Report) {
+  Report.validatesInclusionOf('generalSafety', {
+    in: [1,2,3,4,5,6,7,8,9,10]
+  });
 
+
+  Report.validatesInclusionOf('cleanliness', {
+    in: [1,2,3,4,5,6,7,8,9,10]
+  });
+
+  Report.validatesLengthOf('comments', {min: 3});
+
+  Report.validatesNumericalityOf('lat', { message: { number: 'is not a number' }});
+
+  Report.validatesNumericalityOf('long', { message: { number: 'is not a number' }});
 };
